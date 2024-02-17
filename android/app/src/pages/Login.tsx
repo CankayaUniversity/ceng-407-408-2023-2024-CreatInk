53c5eec3aae79df9082fe18770fa2aa7bb35eb53
aylinakyol <akyolaylin2@gmail.com>
2024-02-18 00:50:47 +0300

    pembiş

---
+++ android/app/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Login = ()=>{
+    return(
+        <div>Home</div>
+    )
+}
+export default Login 
---
+++ android/app/src/pages/Register.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Register = ()=>{
+    return(
+        <div>Home</div>
+    )
+}
+export default Register
