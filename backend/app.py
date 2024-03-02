393682c099b9f2be1d8cd800652aaed76108f844
Çağdaş Terzi <cadomado@windowslive.com>
2024-03-02 23:21:12 +0300

    db init

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,29 @@
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:111852@localhost/creatink'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default = datetime.datetime.now)
+
+    def __init__ (self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+
 
 @app.route("/")
 def get_articles():
