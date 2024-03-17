d41a73e7a642e4fdd1c1aa0a881661fd531e9c05
aylinakyol <akyolaylin2@gmail.com>
2024-03-17 14:47:37 +0300

    Merge branch 'main' of https://github.com/CankayaUniversity/ceng-407-408-2023-2024-CreatInk

--- AppProject/App.js
+++ AppProject/App.js
@@ -14,58 +14,46 @@ import SplashScreen from './Screen/SplashScreen';
 import LoginScreen from './Screen/LoginScreen';
 import RegisterScreen from './Screen/RegisterScreen';
 import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
+import TestScreen from './Screen/TestScreen';
+import HomeScreen from './Screen/DrawerScreens/HomeScreen';
 
 const Stack = createStackNavigator();
 
-const Auth = () => {
-  // Stack Navigator for Login and Sign up Screen
-  return (
-    <Stack.Navigator initialRouteName="LoginScreen">
-      <Stack.Screen
-        name="LoginScreen"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="RegisterScreen"
-        component={RegisterScreen}
-        options={{
-          title: 'Register', //Set Header Title
-          headerStyle: {
-            backgroundColor: '#cc30a0', //Set Header color
-          },
-          headerTintColor: '#fff', //Set Header text color
-          headerTitleStyle: {
-            fontWeight: 'bold', //Set Header text style
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
-};
+// const Auth = () => {
+//   // Stack Navigator for Login and Sign up Screen
+//   return (
+//     <Stack.Navigator initialRouteName="LoginScreen">
+//       <Stack.Screen
+//         name="LoginScreen"
+//         component={LoginScreen}
+//         options={{ headerShown: false }}
+//       />
+//       <Stack.Screen
+//         name="RegisterScreen"
+//         component={RegisterScreen}
+//         options={{
+//           title: 'Register', //Set Header Title
+//           headerStyle: {
+//             backgroundColor: '#cc30a0', //Set Header color
+//           },
+//           headerTintColor: '#fff', //Set Header text color
+//           headerTitleStyle: {
+//             fontWeight: 'bold', //Set Header text style
+//           },
+//         }}
+//       />
+//     </Stack.Navigator>
+//   );
+// };
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SplashScreen">
-        {/* SplashScreen which will come once for 5 Seconds */}
+
         <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          // Hiding header for Splash Screen
-          options={{ headerShown: false }}
-        />
-        {/* Auth Navigator: Include Login and Signup */}
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{ headerShown: false }}
-        />
-        {/* Navigation Drawer as a landing page */}
-        <Stack.Screen
-          name="DrawerNavigationRoutes"
-          component={DrawerNavigationRoutes}
-          // Hiding header for Navigation Drawer
+          name="TestScreen"
+          component={TestScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
@@ -74,3 +62,24 @@ const App = () => {
 };
 
 export default App;
+
+//  {/* SplashScreen which will come once for 5 Seconds */}
+//  <Stack.Screen
+//  name="SplashScreen"
+//  component={SplashScreen}
+//  // Hiding header for Splash Screen
+//  options={{ headerShown: false }}
+// />
+// {/* Auth Navigator: Include Login and Signup */}
+// <Stack.Screen
+//  name="Auth"
+//  component={Auth}
+//  options={{ headerShown: false }}
+// />
+// {/* Navigation Drawer as a landing page */}
+// <Stack.Screen
+//  name="DrawerNavigationRoutes"
+//  component={DrawerNavigationRoutes}
+//  // Hiding header for Navigation Drawer
+//  options={{ headerShown: false }}
+// /> */}
---
+++ AppProject/Screen/TestScreen.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    FlatList,
+    Button
+} from 'react-native';
+import { Card } from 'react-native-paper';
+
+function TestScreen() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('http://192.168.1.36:5000/getAllClients', {
+            method: 'GET'
+        })
+            .then(resp => resp.json())
+            .then(client => {
+                setData(client)
+            })
+    }, [])
+
+    const renderData = (item) => {
+        return (
+            <Card>
+                <Text>{item.name}</Text>
+                <Text>{item.phone}</Text>
+                <Text>{item.email}</Text>
+            </Card>
+        )
+    }
+
+    return (
+        <View>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => {
+                    return renderData(item)
+                }}
+                keyExtractor={item => `${item.id}`}
+            />
+        </View>
+    )
+};
+
+export default TestScreen;
+
+const styles = StyleSheet.create({
+    mainBody: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#cc30a0',
+        alignContent: 'center',
+    },
+    SectionStyle: {
+        flexDirection: 'row',
+        height: 40,
+        marginTop: 20,
+        marginLeft: 35,
+        marginRight: 35,
+        margin: 10,
+    },
+    buttonStyle: {
+        backgroundColor: '#7DE24E',
+        borderWidth: 0,
+        color: '#FFFFFF',
+        borderColor: '#7DE24E',
+        height: 40,
+        alignItems: 'center',
+        borderRadius: 30,
+        marginLeft: 35,
+        marginRight: 35,
+        marginTop: 20,
+        marginBottom: 25,
+    },
+    buttonTextStyle: {
+        color: '#FFFFFF',
+        paddingVertical: 10,
+        fontSize: 16,
+    },
+    inputStyle: {
+        flex: 1,
+        color: 'black',
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderWidth: 1,
+        borderRadius: 30,
+        borderColor: '#dadae8',
+    },
+    registerTextStyle: {
+        color: 'black',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14,
+        alignSelf: 'center',
+        padding: 10,
+    },
+    errorTextStyle: {
+        color: 'red',
+        textAlign: 'center',
+        fontSize: 14,
+    },
+});
