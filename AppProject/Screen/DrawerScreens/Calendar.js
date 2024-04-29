ab18b893900aa12a4e4382952aad168f327d683c
Rifattasli <rifathantasli@gmail.com>
2024-04-29 16:54:39 +0300

    Calendar and Agenda

--- AppProject/Screen/DrawerScreens/Calendar.js
+++ AppProject/Screen/DrawerScreens/Calendar.js
@@ -2,49 +2,77 @@
 // https://aboutreact.com/react-native-login-and-signup/
 
 // Import React and Component
-import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import React,{useState} from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import {Card, Avatar} from 'react-native-paper';
+import {LocaleConfig} from 'react-native-calendars';
 
-const Calendar = () => {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ flex: 1, padding: 16 }}>
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <Text
-                        style={{
-                            fontSize: 20,
-                            textAlign: 'center',
-                            marginBottom: 16,
-                        }}>
-                        This screen shows your appointments
-                        {'\n\n'}
-                        
-                    </Text>
-                </View>
-                <Text
-                    style={{
-                        fontSize: 18,
-                        textAlign: 'center',
-                        color: '#d8d8d8',
-                    }}>
-                    
-                </Text>
-                <Text
-                    style={{
-                        fontSize: 16,
-                        textAlign: 'center',
-                        color: '#d8d8d8',
-                    }}>
-                    CreatINK
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
 };
 
-export default Calendar;
+
+const CalendarScreen = () => {
+
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text>{item.name}</Text>
+              <Avatar.Text label="J" />
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        
+        renderItem={renderItem}
+      />
+    </View>
+  );
+
+};
+
+export default CalendarScreen;
